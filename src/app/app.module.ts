28b720e63a2381e06422fb062be8870a8877e57f
Anderson Dias <andersoninfonet@gmial.com>
2023-07-08 00:42:34 -0300

    feat-creating routes and layout components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,31 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomeComponent } from './pages/home/home.component';
+import { FilmesComponent } from './pages/filmes/filmes.component';
+import { NaveComponent } from './pages/nave/nave.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { ToolBarComponent } from './components/tool-bar/tool-bar.component';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    FilmesComponent,
+    NaveComponent,
+    ToolBarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
